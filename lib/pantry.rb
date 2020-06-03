7ef256d2710e156c99264bfe6e0cdd32e739edb9
Sienna Kopf <princess.kopf@gmail.com>
2020-06-03 12:06:15 -0600

    Add enough ingredients for method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,8 +1,10 @@
 class Pantry
-  attr_reader :stock
+  attr_reader :stock,
+              :enough_ingredients
 
   def initialize
     @stock = Hash.new(0)
+    @enough_ingredients = false
   end
 
   def stock_check(ingredient)
@@ -10,7 +12,25 @@ class Pantry
   end
 
   def restock(ingredient, amount)
-    @stock[ingredient] += amount 
+    @stock[ingredient] += amount
+  end
+
+  def enough_ingredients_for?(recipe)
+    stock_items_with_appropriate_amount = []
+    @stock.each do |stock_ingredient, stock_amount|
+       if @stock[stock_ingredient] >= recipe.ingredients_required[stock_ingredient]
+         stock_items_with_appropriate_amount << stock_ingredient
+       end
+    end
+
+    if stock_items_with_appropriate_amount.nil?
+      @enough_ingredients = false
+    elsif recipe.ingredients == stock_items_with_appropriate_amount
+      @enough_ingredients = true
+    else
+      @enough_ingredients = false
+    end
+    @enough_ingredients
   end
 
 end
