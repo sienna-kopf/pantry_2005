2dd106956defb32404d4fe4274c539cb11646f68
Sienna Kopf <princess.kopf@gmail.com>
2020-06-03 10:44:07 -0600

    Add setup and test for adding ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -8,6 +8,10 @@ class RecipeTest < Minitest::Test
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @recipe1 = Recipe.new("Mac and Cheese")
+
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
   end
 
   def test_it_exists
@@ -18,6 +22,15 @@ class RecipeTest < Minitest::Test
     another_recipe = Recipe.new("Grilled Cheese")
     assert_equal "Grilled Cheese", another_recipe.name
     expected = Hash.new(0)
-    assert_equal expected, another_recipe.ingredients_required 
+    assert_equal expected, another_recipe.ingredients_required
+  end
+
+  def test_it_can_add_ingredients
+    expected = {
+      @ingredient1 => 6,
+      @ingredient2 => 8
+    }
+    assert_equal expected, @recipe1.ingredients_required
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients 
   end
 end
