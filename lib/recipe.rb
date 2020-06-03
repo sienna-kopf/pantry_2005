568301fb30c4899ee95e3f3b98c1909e7c846644
Sienna Kopf <princess.kopf@gmail.com>
2020-06-03 10:49:33 -0600

    Add method for add ingredient to ingredients array and ingredients_required hash

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,10 +1,18 @@
 class Recipe
   attr_reader :name,
-              :ingredients_required
+              :ingredients_required,
+              :ingredients
 
   def initialize(name)
     @name = name
+    @ingredients = []
     @ingredients_required = Hash.new(0)
   end
 
+  def add_ingredient(ingredient, amount)
+    @ingredients_required[ingredient] += amount
+    if !(@ingredients.include?(ingredient))
+      @ingredients << ingredient
+    end
+  end 
 end
