e7ea6e6a0326b0dc6f5dc79aa879beb451dd1ad7
Sienna Kopf <princess.kopf@gmail.com>
2020-06-03 11:14:05 -0600

    Add test for cookbook ingredients

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -29,9 +29,30 @@ class CookBookTest < Minitest::Test
   def test_it_is_initialized_with_no_recipes
     another_cookbook = CookBook.new
     assert_equal [], another_cookbook.recipes
+    assert_equal [], another_cookbook.ingredients
   end
 
   def test_it_can_add_recipes
     assert_equal [@recipe1, @recipe2],  @cookbook.recipes
+  end
+
+  def test_it_can_return_ingredients_in_recipes
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook = CookBook.new 
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], cookbook.ingredients
   end
 end
